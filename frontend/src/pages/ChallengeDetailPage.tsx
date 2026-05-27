f9bf1a824a65f3d1543516415a4f648a15753ea2
ahsansuryana <ahsansuryana.12252006@gmail.com>
2026-05-27 19:58:57 +0700

    fix: remove unused useNavigate import in ChallengeDetailPage

--- frontend/src/pages/ChallengeDetailPage.tsx
+++ frontend/src/pages/ChallengeDetailPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { AxiosError } from 'axios';
 import { motion } from 'framer-motion';
 import api from '../lib/api';
